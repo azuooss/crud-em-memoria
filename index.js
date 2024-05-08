9ddfa2843c4b76dbd1ee97d9ad64dc85cecb11df
Victor Matias <vimatiassouza@gmail.com>
2024-05-08 21:32:44 +0000

    Checagem de item existente, retornando 404

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ app.get('/personagem/:id', function(req,res)
 
     // Acessa o item na lista usando o ID - 1
     const item = lista[id - 1]
+
+    // Checamos se o item é existente
+    if (!item)
+    {
+        return res.status(404).send('Item não encontrado.')
+    }
     
     // Enviamos o item como resposta
     res.send(item)
@@ -43,20 +49,20 @@ app.post('/personagem', function (req,res)
     // Checar se o 'nome' está presente no body
     if(!novoItem)
     {
-        return res.send('Corpo da requisição deve conter a propriedade "nome".')
+        return res.status(400).send('Corpo da requisição deve conter a propriedade "nome".')
     }
 
     // Checar se o novoItem está na lista ou não
     if(lista.includes(novoItem))
     {
-        return res.send('Item já existe na lista')
+        return res.status(409).send('Item já existe na lista')
     }
     
     // Adicionamos na lista
     lista.push(novoItem)
 
     // Exibimos uma mensagem de sucesso
-    res.send('Item adicionado com sucesso: ' + novoItem)
+    res.status(201).send('Item adicionado com sucesso: ' + novoItem)
     
 })
 
@@ -67,6 +73,12 @@ app.put('/personagem/:id', function(req,res)
     // Acessamos o ID dos parâmetros de rota
     const id = req.params.id
 
+    //Checamos se o ID - 1 está na lista
+    if (!lista[id - 1])
+    {
+        return res.status(404).send('Item não encontrado.')
+    }
+
     // Acessamos o Body da requisição
     const body = req.body
 
@@ -76,20 +88,20 @@ app.put('/personagem/:id', function(req,res)
     // Checar se o 'nome' está presente no body
     if(!novoItem)
     {
-        return res.send('Corpo da requisição deve conter a propriedade "nome".')
+        return res.status(400).send('Corpo da requisição deve conter a propriedade "nome".')
     }
 
     // Checar se o novoItem está na lista ou não
     if(lista.includes(novoItem))
     {
-        return res.send('Item já existe na lista')
+        return res.status(409).send('Item já existe na lista')
     }  
-      
+
     // Atualizamos na lista o novoItem pelo ID - 1
     lista[id - 1] = novoItem
 
     // Enviamos uma mensagem de sucesso
-    res.send('Item atualizado com sucesso: ' + id  + ' - ' + novoItem)
+    res.status(201).send('Item atualizado com sucesso: ' + id  + ' - ' + novoItem)
 })
 
 // Endpoint Delete [DELETE] /personagem/:id
@@ -97,6 +109,12 @@ app.delete('/personagem/:id', function (req, res)
 {
     // Acessamos o parâmetro de rota
     const id = req.params.id
+
+    //Checamos se o item obtido é existente
+    if (!lista[id - 1])
+    {
+        return res.status(404).send('Item não encontrado.')
+    }
 
     // Remover o item da lista usando o ID - 1
     delete lista[id - 1]
