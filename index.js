b9db9be5affe8aa4cbf1f01f054f5e0f2cdd2433
Victor Matias <vimatiassouza@gmail.com>
2024-05-08 20:08:01 +0000

    Endpoint 'Read All [GET] /personagem' implementado

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ app.get('/', function (req, res)
     res.send('Hello World!')
 })
 
+const lista = ['Java','Kotlin', 'Android']
+
+// Endpoint Read All [GET] /personagem
+
+app.get('/personagem', function(req,res)
+{
+    res.send(lista)
+})
+
+
 app.listen(3000)
